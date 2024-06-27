94324f44058c5b25ea178a85d5ec62000f60a850
Gauhar <asanbayg@gmail.com>
2024-06-28 01:47:16 +0500

    feat: read xlsx files

--- src/components/ImportFile.jsx
+++ src/components/ImportFile.jsx
@@ -1,24 +1,39 @@
 import { useState } from "react";
 import ToggleDiv from "./ui/ToggleDiv";
 import Papa from "papaparse";
+import readXlsxFile from "read-excel-file";
 
 export const ImportFile = () => {
   const [faq, setFaq] = useState([]);
 
-  const handleCsvUpload = (event) => {
+  const handleFileUpload = (event) => {
     const file = event.target.files[0];
-    if (file) {
+    const fileType = file.name.split(".");
+    const extension = fileType[fileType.length - 1];
+
+    if (extension === "csv") {
       parseCSV(file);
+    } else if (extension === "xlsx") {
+      parseXlsx(file);
     }
   };
 
   const parseCSV = (file) => {
     Papa.parse(file, {
       complete: function (res) {
-        console.log(res.data);
         setFaq(res.data);
       },
       header: true,
+    });
+  };
+
+  const parseXlsx = (file) => {
+    readXlsxFile(file).then((rows) => {
+      const faqArray = rows.slice(1).map((row) => ({
+        Question: row[0],
+        Answer: row[1],
+      }));
+      setFaq(faqArray);
     });
   };
 
@@ -27,7 +42,7 @@ export const ImportFile = () => {
       <p className="font-bold">
         Note: table should have two headers - Question and Answer
       </p>
-      <input type="file" accept=".csv" onChange={handleCsvUpload} />
+      <input type="file" accept=".csv, .xlsx" onChange={handleFileUpload} />
       <ul>
         {faq.map((item, index) => (
           <li key={index}>
