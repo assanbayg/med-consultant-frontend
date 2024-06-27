6161d3b0d350071c2e9a69dabf1ce72934d319d0
Gauhar <asanbayg@gmail.com>
2024-06-28 02:05:25 +0500

    feat: upload dialog example as txt

--- src/components/ImportFile.jsx
+++ src/components/ImportFile.jsx
@@ -5,11 +5,26 @@ import readXlsxFile from "read-excel-file";
 
 export const ImportFile = () => {
   const [faq, setFaq] = useState([]);
+  const [dialogueExample, setDialogueExample] = useState("");
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
     const fileType = file.name.split(".");
     const extension = fileType[fileType.length - 1];
+
+    if (extension === "txt") {
+      console.log(file);
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target.result;
+        const dialogueArray = text
+          .split("\n")
+          .map((line) => line.trim())
+          .filter((line) => line);
+        setDialogueExample(dialogueArray);
+      };
+      reader.readAsText(file);
+    }
 
     if (extension === "csv") {
       parseCSV(file);
@@ -38,19 +53,41 @@ export const ImportFile = () => {
   };
 
   return (
-    <ToggleDiv title={"Import CSV file"} isTitle={true}>
-      <p className="font-bold">
-        Note: table should have two headers - Question and Answer
-      </p>
-      <input type="file" accept=".csv, .xlsx" onChange={handleFileUpload} />
-      <ul>
-        {faq.map((item, index) => (
-          <li key={index}>
-            <strong>Q:</strong> {item.Question} <br />
-            <strong>A:</strong> {item.Answer}
-          </li>
-        ))}
-      </ul>
+    <ToggleDiv title={"Import files"} isTitle={true}>
+      <div className="flex flex-col max-w-2xl gap-y-4">
+        <div className="flex gap-x-2 justify-start">
+          <label htmlFor="txtFile">Import dialog example (.txt)</label>
+          <input
+            type="file"
+            name="txtFile"
+            id="txtFile"
+            accept=".txt"
+            onChange={handleFileUpload}
+          />
+          <p>{dialogueExample}</p>
+        </div>
+        <div className="flex gap-x-2 justify-start">
+          <label htmlFor="tableFile">
+            Import QA example (.csv, .xlsx). Should have two headers - Question
+            and Answer
+          </label>
+          <input
+            type="file"
+            accept=".csv, .xlsx"
+            name="tableFile"
+            id="tableFile"
+            onChange={handleFileUpload}
+          />
+        </div>
+        <ul>
+          {faq.map((item, index) => (
+            <li key={index}>
+              <strong>Q:</strong> {item.Question} <br />
+              <strong>A:</strong> {item.Answer}
+            </li>
+          ))}
+        </ul>
+      </div>
     </ToggleDiv>
   );
 };
